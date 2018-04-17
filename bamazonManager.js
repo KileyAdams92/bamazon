bf173fe296c2c47b6baaf7f284b0e3d758c440fa
Kiley Adams <Kileyadams92@gmail.com>
2018-04-16 21:26:53 -0500

    created funcitons for adding new inventory and adding new product

--- bamazonManager.js
+++ bamazonManager.js
@@ -46,10 +46,10 @@ function listOptions() {
           return lowInventory();
 
         case "Add to Inventory":
-          return;
+          return addToCurrent();
 
         case "Add New Product":
-          return;
+          return addNewProduct();
       }
       //   connection.query(
       //     "SELECT * FROM products WHERE ?",
@@ -113,13 +113,141 @@ function listOptions() {
     var currentStockQuantity;
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
+      console.log(
+        "The following products are considered to have low inventory:"
+      );
       for (var i = 0; i < res.length; i++) {
         currentStockQuantity = res[i].stock_quantity;
         if (currentStockQuantity < 5) {
-          console.log(res[i]);
+          console.table(res[i].product_name);
         }
       }
     });
+
     connection.end();
   }
+
+  function addToCurrent() {
+    var currentStockQuantity;
+    connection.query("SELECT * FROM products", function(err, res) {
+      console.log(
+        "----------------------------------------------------------------------"
+      );
+      console.table(res);
+      if (err) throw err;
+      inquirer
+        .prompt([
+          {
+            name: "item",
+            type: "input",
+            message:
+              "What is the ID of the item you would like to add inventory to?"
+          },
+          {
+            name: "inventoryAdd",
+            type: "input",
+            message:
+              "How much inventory would you like to add to the existing product?"
+          }
+        ])
+        .then(function(answers) {
+          connection.query(
+            "SELECT * FROM products WHERE ?",
+            { id: answers.item },
+            // "UPDATE products SET ? WHERE ?",[
+            //   {stock_quantity = currentStockQuantity + answers.inventoryAdd},
+            //   { id: answers.item }
+            // ],
+            function(err, res) {
+              if (err) throw err;
+              var newStockQuantity;
+              for (var i = 0; i < res.length; i++) {
+                currentStockQuantity = res[i].stock_quantity;
+                console.log(currentStockQuantity);
+              }
+              if (currentStockQuantity >= 0) {
+                connection.query(
+                  "UPDATE products SET ? WHERE ?",
+                  [
+                    {
+                      stock_quantity:
+                        currentStockQuantity + parseInt(answers.inventoryAdd)
+                    },
+                    {
+                      id: answers.item
+                    }
+                  ],
+                  function(err, res) {
+                    if (err) throw err;
+                    if (!err) {
+                      console.log("Your inventory has been updated");
+                      connection.end();
+                    } else {
+                      console.log(
+                        "You may need to update your inventory at a different time."
+                      );
+                      connection.end();
+                    }
+                  }
+                );
+              }
+            }
+          );
+        });
+    });
+  }
+
+  function addNewProduct() {
+    inquirer
+      .prompt([
+        {
+          name: "id",
+          type: "input",
+          message: "What is the next ID in the list of products "
+        },
+        {
+          name: "productName",
+          type: "input",
+          message: "What is the name of the product you would like to add?"
+        },
+        {
+          name: "department",
+          type: "input",
+          message: "What department is the product going to be sold in?"
+        },
+        {
+          name: "price",
+          type: "input",
+          message: "How much does the product cost?"
+        },
+        {
+          name: "stockQuantity",
+          type: "input",
+          message: "How much of this item are you adding to the inventory?"
+        }
+      ])
+      .then(function(answers) {
+        connection.query(
+          "INSERT INTO products SET ?",
+          {
+            id: id,
+            product_name: productName,
+            department_name: department,
+            price: price,
+            stock_quantity: stockQuantity
+          },
+          function(err, res) {
+            if (err) throw err;
+            if (!err) {
+              console.log("Your product has been added to the store!");
+            } else {
+              console.log(
+                "Technical difficulties have occurred. Please try to add your item at a later date."
+              );
+            }
+          }
+        );
+      });
+  }
+  connection.end();
 }
